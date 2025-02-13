ebefd39a21fd49aacb331e3bcda7b9fecdb906fa
Meku-nint <mw908334@gmail.com>
2025-02-13 10:33:12 +0300

    the the second commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import react from 'react'
+import reactDOM from'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root=reactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <div>
     <App/>
-  </React.StrictMode>
-);
-reportWebVitals();
+  </div>
+)
---
+++ src/pages/HomePage.js
@@ -0,0 +1,41 @@
+import React from 'react'
+const HomePage = () => {
+  return (
+    <div className='homePage'>
+        <nav className='navigation-bar'>
+            <div className='navigation-list'>
+                 <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Login</li>
+                 </ul>
+            </div>
+        </nav>
+        <div className='tellme'>
+          <p>Track Your Progress, Elevate Your Potential</p>
+        </div>
+             <div className='images'>
+                <img src="https://t3.ftcdn.net/jpg/01/40/53/12/360_F_140531219_chCHBmALfRuKyyfZasWWT8eIM4f6wC2P.jpg" alt="Productivity Tools" width="100%"/>  
+                <img src="https://www.proofhub.com/articles/wp-content/uploads/2023/11/Productivity-Tools-That-Will-Make-Your-Life-Much-Easier.jpg" alt="Productivity Tools" width="100%"/>   
+            </div>
+                <div className='tellme-header'><h3>Tell me Your day</h3><p><strong>your personal productivity tracker</strong></p></div>
+        <div className='container'>
+          <div className='use1'>
+              <p><strong>✅ Track Your Day</strong></p>
+              <p>With the TellMe App, you can effortlessly log what you’ve achieved each day—whether it's a work task, a personal goal, or simply something you enjoy. The app helps you categorize and track your activities in real time, giving you a detailed overview of your day. By visually seeing what you’ve accomplished, you’ll feel motivated to keep up the momentum and make the most out of your time.</p>
+          </div>
+          <div className='use2'>
+              <p><strong>✅ Stay Accountable</strong></p>
+              <p>Reflecting on your daily achievements and challenges allows you to hold yourself accountable. The TellMe App encourages you to review your progress at the end of each day, helping you identify areas of strength and areas where improvement is needed. Whether you’ve hit your targets or missed some, you'll have the clarity to adjust your actions and stay on track toward your long-term goals.</p>
+          </div>
+          <div className='use3'>
+              <p><strong>✅ Maximize Productivity</strong></p>
+              <p>TellMe is more than just a task tracker. It’s a tool that helps you stay organized, plan your time efficiently, and eliminate distractions. The app empowers you to prioritize tasks based on importance, ensuring that you focus on what matters most. Whether you're managing work tasks, personal projects, or leisure activities, TellMe keeps you organized, motivated, and moving forward with a clear direction. Transform your intentions into tangible results by staying productive every day.</p>
+          </div>
+        </div>
+    </div>
+
+  )
+}
+export default HomePage
